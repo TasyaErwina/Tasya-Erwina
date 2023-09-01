de19fd86c178c43463c424df8e39126a93e75890
TasyaErwina <143817464+TasyaErwina@users.noreply.github.com>
2023-09-01 22:23:25 +0700

    Add files via upload

---
+++ Vertical.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MyHomePage(title: 'TASYA ERWINA'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TASYA ERWINA (Vertical Container)'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text('Tasya 1'),
+              onPressed: () {},
+              
+            ),
+            SizedBox(width: 18),
+            ElevatedButton(
+              child: Text('Tasya 2'),
+              onPressed: () {},
+            ),
+            SizedBox(width: 18),
+            ElevatedButton(
+              child: Text('Tasya 3'),
+              onPressed: () {},
+            ),
+            SizedBox(width: 18),
+            ElevatedButton(
+              child: Text('Tasya 4'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
